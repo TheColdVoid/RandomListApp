1683093f23496b892c354a38b4d1f502df143214
shepherXD <1046019173@qq.com>
2019-07-13 21:48:05 +0800

    nromal webpage without certain function

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-document.write("it work!");
+class Go {
+  constructor(userGo) {
+    this.message = userGo;
+    this.id = vm.id++;
+  }
+}
+var vm = new Vue({
+  el: "#randomGo",
+  data: {
+    randomList: [{ message: "gogogogogogo!" }, { message: "que!" }],
+    userGo: null,
+    id: 1
+  },
+  methods: {
+    addToList: function() {
+      this.randomList.push(new Go(this.userGo));
+      this.userGo = null;
+    }
+  }
+});
