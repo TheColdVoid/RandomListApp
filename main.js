0ec60ed89de1fbbee5b5324e17e4aa0df12d9610
shepherXD <1046019173@qq.com>
2019-07-16 01:28:15 +0800

    fix the prolems

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 var vm = new Vue({
   el: "#randomGo",
   data: {
-    randomList: [{ message: "gogogogogogo!" }, { message: "que!" }],
+    randomList: [],
     resultList: [],
     userGo: "",
-    id: 1,
+    id: 0,
     count: null
+  },
+  components: {
+    'random-list': {
+      props: ['go'],
+      methods: {
+        callDelGo: function(id){
+          this.$emit('call-delgo',id);
+        }
+      },
+      template: '<div><li>{{go.message}}</li><button v-on:click="callDelGo(go.id)">x</button></div>'
+    }
   },
   methods: {
     addToList: function() {
@@ -18,6 +29,13 @@ var vm = new Vue({
     },
     showResult: function() {
       this.resultList = randomChoice(this.count,this.randomList);
+    },
+    delGo: function(id){
+      this.randomList.splice(id,1);
+      var i;
+      for(i=0;i<this.randomList.length;i++){     // 遍历重排id值，我也想用map啊嘤嘤嘤
+        this.randomList[i].id=i;
+      }
     }
   }
 });
