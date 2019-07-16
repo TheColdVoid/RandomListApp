431b9e69dcba1fa97e8ccbe568635299b06082bf
shepherXD <1046019173@qq.com>
2019-07-16 19:21:03 +0800

    revise the problems //delete id

--- main.js
+++ main.js
@@ -11,16 +11,16 @@ var vm = new Vue({
     'random-list': {
       props: ['go'],
       methods: {
-        onRemoveGo: function(id){
-          this.$emit('remove-go',id);
+        onRemoveGo: function(message){
+          this.$emit('remove-go',message);
         }
       },
-      template: '<div><li>{{go.message}}</li><button v-on:click="onRemoveGo(go.id)">x</button></div>'
+      template: '<div><li>{{go}}</li><button v-on:click="onRemoveGo(go)">x</button></div>'
     }
   },
   methods: {
     addToList: function() {
-      this.randomList.push({ message: this.userGo, id: this.id++ });
+      this.randomList.push(this.userGo);
       this.userGo = null;
     },
     randomChoice: function(count,arrayGo) {
@@ -30,12 +30,9 @@ var vm = new Vue({
     showResult: function() {
       this.resultList = randomChoice(this.count,this.randomList);
     },
-    removeGo: function(id){
-      this.randomList.splice(id,1);
-      var i;
-      for(i=0;i<this.randomList.length;i++){     // 遍历重排id值，我也想用map啊嘤嘤嘤
-        this.randomList[i].id=i;
+    removeGo: function(message){
+      this.randomList = this.randomList.filter(it => it !== message);
+      
       }
     }
-  }
-});
+  })
