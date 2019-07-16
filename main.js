254e0367d8b9c60fa1f8bb76c5aa923d2ef15382
shepherXD <1046019173@qq.com>
2019-07-16 22:40:29 +0800

    修改了 1。抽选数无默认值；2.两个list区分不明显；3.交互不明显 的问题

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 var vm = new Vue({
   el: "#randomGo",
   data: {
-    randomList: [],
+    currList: [],
     resultList: [],
-    userGo: "",
+    newGo: "",
     id: 0,
-    count: null
+    count: 1
   },
   components: {
     'random-list': {
@@ -15,19 +15,23 @@ var vm = new Vue({
           this.$emit('remove-go',message);
         }
       },
-      template: '<div><li>{{go}}</li><button v-on:click="onRemoveGo(go)">x</button></div>'
+      template: '<div><p>{{go}}</p><button v-on:click="onRemoveGo(go)">x</button></div>'
     }
   },
   methods: {
     addToList: function() {
-      this.randomList.push(this.userGo);
-      this.userGo = null;
+      this.currList.push(this.newGo);
+      this.newGo = null;
     },
-    showResult: function() {
-      this.resultList = randomChoice(this.count,this.randomList);
+    randomChoice: function(count,arrayGo) {
+      var arrangedList;
+      return arrangedList;//输出随机排好的数组
+    },
+    setResult: function() {
+      this.resultList = randomChoice(this.count,this.currList);
     },
     removeGo: function(message){
-      this.randomList = this.randomList.filter(it => it !== message);
+      this.currList = this.currList.filter(it => it !== message);
 
       }
     }
